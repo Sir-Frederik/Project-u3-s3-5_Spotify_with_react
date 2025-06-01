d0120d834d30e77112d670d5858bb7a371660b50
Federico <brunetti.federico.13@gmail.com>
2025-06-01 18:24:25 +0200

    collefagto favourite page a maincard

--- src/components/FavouritesPage.jsx
+++ src/components/FavouritesPage.jsx
@@ -1,18 +1,17 @@
-import { Col, Image, Row } from "react-bootstrap";
-import { Star, StarFill } from "react-bootstrap-icons";
+import { Col, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
+import MainCard from "./MainCard"; // importa MainCard
 
 const FavouritesPage = () => {
   const favSongs = useSelector((state) => state.favourites.content);
   const dispatch = useDispatch();
 
-  const handleClick = (data) => {
-    dispatch({ type: "SELECT_SONG", payload: data });
+  const onSelectSong = (song) => {
+    dispatch({ type: "SELECT_SONG", payload: song });
   };
 
-  const handleFav = (song) => {
+  const onToggleFavourite = (song) => {
     const favSongIds = favSongs.map((obj) => obj.id);
-
     if (favSongIds.includes(song.id)) {
       dispatch({ type: "REMOVE_FROM_FAV", payload: song.id });
     } else {
@@ -21,57 +20,28 @@ const FavouritesPage = () => {
   };
 
   return (
-    <>
-      <Col className="col-12 col-md-9 offset-md-3 mainPage">
-        <Row>
-          <Col className="col-9 col-lg-11 mainLinks d-none d-md-flex">
-            <a href="#">TRENDING</a>
-            <a href="#">PODCAST</a>
-            <a href="#">MOODS AND GENRES</a>
-            <a href="#">NEW RELEASES</a>
-            <a href="#">DISCOVER</a>
-          </Col>
-        </Row>
-        <Row className="mt-5">
-          <Col className="col-10">
-            <Row className="hipHopSection">
-              {favSongs.map((song) => (
-                <Col className="col-3 text-center d-flex flex-column align-items-center" key={song.id}>
-                  <div className="position-relative imgContainer">
-                    {favSongs.includes(song) ? (
-                      <StarFill
-                        className="star"
-                        onClick={() => {
-                          handleFav(song);
-                        }}
-                      />
-                    ) : (
-                      <Star
-                        className="star"
-                        onClick={() => {
-                          handleFav(song);
-                        }}
-                      />
-                    )}
-                    <Image
-                      className=""
-                      fluid
-                      src={song.album.cover_medium}
-                      alt="track"
-                      onClick={() => {
-                        handleClick(song);
-                      }}
-                    />
-                  </div>
-                  <p>"{song.title}"</p>
-                  <p> {song.artist.name}</p>
-                </Col>
-              ))}
-            </Row>
-          </Col>
-        </Row>
-      </Col>
-    </>
+    <Col className="col-12 col-md-9 offset-md-3 mainPage">
+      <Row>
+        <Col className="col-9 col-lg-11 mainLinks d-none d-md-flex">
+          <a href="#">TRENDING</a>
+          <a href="#">PODCAST</a>
+          <a href="#">MOODS AND GENRES</a>
+          <a href="#">NEW RELEASES</a>
+          <a href="#">DISCOVER</a>
+        </Col>
+      </Row>
+      <Row className="mt-5 hipHopSection">
+        {favSongs.map((song) => (
+          <MainCard
+            key={song.id}
+            song={song}
+            isFavourite={true} // sono tutte favorite in questa pagina
+            onToggleFavourite={onToggleFavourite}
+            onSelectSong={onSelectSong}
+          />
+        ))}
+      </Row>
+    </Col>
   );
 };
 

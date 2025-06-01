212e0da29378be8ec450f9841fa7bfd7225433ad
Federico <brunetti.federico.13@gmail.com>
2025-06-01 18:13:47 +0200

    creata favourite page, migliorato stile

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,70 +1,40 @@
 import { useEffect } from "react";
-import { Col, Image } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSongs } from "../redux/actions";
-import { StarFill, Star } from "react-bootstrap-icons";
+import MainCard from "./MainCard";
 
-const Gallery = ({ title, query }) => {
+const Gallery = ({ title, query, artistArr }) => {
   const dispatch = useDispatch();
-
   const favSongs = useSelector((state) => state.favourites.content);
 
   useEffect(() => {
-    dispatch(fetchSongs("queen"));
-    dispatch(fetchSongs("katyperry"));
-    dispatch(fetchSongs("eminem"));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    dispatch(fetchSongs(query));
   }, []);
 
-  const handleClick = (data) => {
-    dispatch({ type: "SELECT_SONG", payload: data });
+  const handleClick = (song) => {
+    dispatch({ type: "SELECT_SONG", payload: song });
   };
 
   const handleFav = (song) => {
-    const favSongIds = favSongs.map((obj) => obj.id);
-
-    if (favSongIds.includes(song.id)) {
-      dispatch({ type: "REMOVE_FROM_FAV", payload: song.id });
-    } else {
-      dispatch({ type: "ADD_TO_FAV", payload: song });
-    }
+    const isFav = favSongs.some((obj) => obj.id === song.id);
+    dispatch({
+      type: isFav ? "REMOVE_FROM_FAV" : "ADD_TO_FAV",
+      payload: isFav ? song.id : song,
+    });
   };
 
   return (
     <>
       <h2>{title}</h2>
       <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3" id="rockSection">
-        {query.map((song) => (
-          <Col className="text-center d-flex flex-column align-items-center" key={song.id}>
-            <div className="position-relative imgContainer">
-              {favSongs.includes(song) ? (
-                <StarFill
-                  className="star"
-                  onClick={() => {
-                    handleFav(song);
-                  }}
-                />
-              ) : (
-                <Star
-                  className="star"
-                  onClick={() => {
-                    handleFav(song);
-                  }}
-                />
-              )}
-              <Image
-                className=""
-                fluid
-                src={song.album.cover_medium}
-                alt="track"
-                onClick={() => {
-                  handleClick(song);
-                }}
-              />
-            </div>
-            <p>Track: "{song.title}"</p>
-            <p>Artist: {song.artist.name}</p>
-          </Col>
+        {artistArr.map((song) => (
+          <MainCard
+            key={song.id}
+            song={song}
+            isFavourite={favSongs.some((obj) => obj.id === song.id)}
+            onToggleFavourite={handleFav}
+            onSelectSong={handleClick}
+          />
         ))}
       </div>
     </>
